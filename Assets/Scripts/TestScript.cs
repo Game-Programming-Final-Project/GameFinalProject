0d242406df5e670157aff90e2d011fee3c40a3f2
cagatayesilyurt <cagatayesilyurt@gmail.com>
2024-12-12 19:19:32 +0300

    Test commit
    Capsule object was added.

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
+        //test script
     }
 
-    // Update is called once per frame
     void Update()
     {
         
